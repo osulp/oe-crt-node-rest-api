dd49a3d27fd7135b3658495952657c6193e0be49
marcgr <marc.rempel@oregonstate.edu>
2016-01-26 07:43:33 -0800

    Added env.port settings check to support dev and prod

--- oe-crt-node-rest-api/app.js
+++ oe-crt-node-rest-api/app.js
@@ -76,6 +76,10 @@ app.use(function (err, req, res, next) {
     });
 });
 
-app.listen(process.env.PORT);
+//production port settings
+//check if using web.config settings
+if (process.env.virtualDirPath !== undefined) {
+    app.listen(process.env.PORT);
+}
 
 module.exports = app;
