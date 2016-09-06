d7ac124713d03b2a564d59cca9857888622f83be
marcgr <marc.rempel@oregonstate.edu>
2016-09-06 12:07:21 -0700

    add support for geoname to community data queries

--- oe-crt-node-rest-api/controllers/community_data_by_indicator.js
+++ oe-crt-node-rest-api/controllers/community_data_by_indicator.js
@@ -4,9 +4,9 @@ var httpMsgs = require("../core/httpMsgs");
 var utilities = require("../core/utilities");
 var format = "json";
 
-exports.get_community_data_by_indicator = function (req, resp) {       
+exports.get_community_data_by_indicator = function (req, resp) {
     settings.format = req.query.f !== "undefined" ? req.query.f : "json";
-    db.executeSql("exec getCommunityData '" + req.query.geoids + "', '" + req.query.indicators + "';", false, 
+    db.executeSql("exec getCommunityData '" + req.query.geoids + "', '" + req.query.indicators + "';", false,
         function (data, err) {
         if (err) {
             httpMsgs.show500(req, resp, err);
@@ -17,8 +17,8 @@ exports.get_community_data_by_indicator = function (req, resp) {
                 httpMsgs.sendJson(req, resp, data, settings.format);
             }
             else {
-                var _stylePath = (process.env.virtualDirPath !== undefined ? 'public' : '') + '/stylesheets/style.css';                       
-                resp.render('dataTable', { title:"Community Data by Indicator Table", table: utilities.tableMarkup(data, false, null), stylePath: _stylePath });                
+                var _stylePath = (process.env.virtualDirPath !== undefined ? 'public' : '') + '/stylesheets/style.css';
+                resp.render('dataTable', { title:"Community Data by Indicator Table", table: utilities.tableMarkup(data, false, null), stylePath: _stylePath });
             }
         }
     });
@@ -27,7 +27,7 @@ exports.get_community_data_by_indicator = function (req, resp) {
 exports.get_community_data_by_indicator_geoType = function (req, resp) {
     settings.format = req.query.f !== "undefined" ? req.query.f : "json";
     //PARAMS for getIndicatorData in order: 1. indicator, 2.geotypes(adv view only,3.geoids(basic view only),4.geonames(basic view only) Not currently used,5.viewType (basic/advanced)
-    db.executeSql("exec getIndicatorData '" + req.query.indicator + "', '" + req.query.geoType + "';", true, 
+    db.executeSql("exec getIndicatorData '" + req.query.indicator + "', '" + req.query.geoType + "';", true,
         function (data, err) {
         if (err) {
             httpMsgs.show500(req, resp, err);
@@ -38,12 +38,12 @@ exports.get_community_data_by_indicator_geoType = function (req, resp) {
             returnObj.Years = data[1];
             returnObj.GeoTypes = data[3];
             returnObj.Data = data[2];
-            returnObj.GeoYears = data[4];    
+            returnObj.GeoYears = data[4];
             returnObj.RelatedIndicators = data.length > 5 ? data[5] : null;
             returnObj.SubTopicCategories = data.length > 6 ? data[6]: null;
             returnObj.TopicCategories = data.length > 7 ? data[7]: null;
-            
-            
+
+
             if (settings.format === "json" || settings.format === "pjson") {
                 httpMsgs.sendJson(req, resp, returnObj, settings.format);
             }
@@ -58,7 +58,8 @@ exports.get_community_data_by_indicator_geoType = function (req, resp) {
 exports.get_community_data_by_indicator_with_metadata = function (req, resp, viewType) {
     settings.format = req.query.f !== "undefined" ? req.query.f : "json";
     //PARAMS for getIndicatorData in order: 1. indicator, 2.geotypes(adv view only,3.geoids(basic view only),4.geonames(basic view only) Not currently used,5.viewType (basic/advanced)
-    db.executeSql("exec getIndicatorData '" + req.query.indicator + "', '','" + req.query.geoids + "','', '" + viewType + "';", true, 
+
+    db.executeSql("exec getIndicatorData '" + req.query.indicator + "', '','" + req.query.geoids + "','" + (req.query.geonames !== "undefined" ? req.query.geonames : '') + "', '" + viewType + "','db';", true,
         function (data, err) {
         if (err) {
             httpMsgs.show500(req, resp, err);
@@ -68,7 +69,7 @@ exports.get_community_data_by_indicator_with_metadata = function (req, resp, vie
             returnObj.Metadata = data[0];
             returnObj.Years = data[1];
             returnObj.Data = data[2];
-            
+
             if (settings.format === "json" || settings.format === "pjson") {
                 httpMsgs.sendJson(req, resp, returnObj, settings.format);
             }
@@ -84,7 +85,7 @@ exports.get_community_data_by_indicator_with_metadata = function (req, resp, vie
 //exports.get_community_detailed_data_by_indicator_with_metadata = function (req, resp) {
 //    settings.format = req.query.f !== "undefined" ? req.query.f : "json";
 //    //PARAMS for getIndicatorData in order: 1. indicator, 2.geotypes(adv view only,3.geoids(basic view only),4.geonames(basic view only) Not currently used,5.viewType (basic/advanced)
-//    db.executeSql("exec getIndicatorData '" + req.query.indicator + "', '','" + req.query.geoids + "','','combined';", true, 
+//    db.executeSql("exec getIndicatorData '" + req.query.indicator + "', '','" + req.query.geoids + "','','combined';", true,
 //        function (data, err) {
 //        if (err) {
 //            httpMsgs.show500(req, resp, err);
@@ -94,7 +95,7 @@ exports.get_community_data_by_indicator_with_metadata = function (req, resp, vie
 //            returnObj.Metadata = data[0];
 //            returnObj.Years = data[1];
 //            returnObj.Data = data[2];
-            
+
 //            if (settings.format === "json" || settings.format === "pjson") {
 //                httpMsgs.sendJson(req, resp, returnObj, settings.format);
 //            }
@@ -107,7 +108,7 @@ exports.get_community_data_by_indicator_with_metadata = function (req, resp, vie
 //}
 
 exports.get = function (req, resp, community) {
-    
+
     db.executeSql("Select * from Communities", function (data, err) {
         if (err) {
             httpMsgs.show500(req, resp, err);
