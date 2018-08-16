7e6feacc758baf9130a82c13a81130f5436f546f
Marc G Rempel <marc.rempel@oregonstate.edu>
2018-08-16 14:46:55 -0700

    Release update to db version 5 and minor updates

--- oe-crt-node-rest-api/settings.js
+++ oe-crt-node-rest-api/settings.js
@@ -3,13 +3,13 @@
     "password": 'Rural!$OutThere',
     "server": 'lib-mssql1.library.oregonstate.edu', // You can use 'localhost\\instance' to connect to named instance 
     "port": "1434",
-    "database": 'community_reporter_4_1',
+    "database": 'community_reporter_5',
     //stream: true, // You can enable streaming globally 
-    
+
     "options": {
         //encrypt: true // Use this if you're on Windows Azure 
     }
-}
+};
 // add needed headers
 var headers = {};
 headers["Access-Control-Allow-Origin"] = "*";
